6cfabbab6c0c7556f52a4463bb1215405c70c081
tpanda1983 <tpandatpanda1983@gmail.com>
2023-02-21 16:48:51 +0200

    create burger menu

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,12 @@ $(document).ready(function(){
     });
 
 });
+
+
+var trigger = document.getElementById('toggle');
+var box = document.getElementById('menu');
+
+toggle.addEventListener('click', function() {
+    box.classList.toggle('active');
+    // $('body').toggleClass('lock');
+});
